e11c58fdee186023a61e71effb61ee5a2010774a
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2024-04-01 21:49:22 +0300

    create and import components

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li class="minus">
+      Rent <span>- $260.00</span>
+      <button class="delete-btn"></button>
+    </li>
+    <li class="plus">
+      Cash <span>+ $400.00</span>
+      <button class="delete-btn"></button>
+    </li>
+  </ul>
+</template>

f2c6fcb89f429828b717053d578bab00e3a80f5c
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2024-04-02 05:37:13 +0300

    transactions in global state

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,13 +1,13 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Rent <span>- $260.00</span>
-      <button class="delete-btn"></button>
-    </li>
-    <li class="plus">
-      Cash <span>+ $400.00</span>
-      <button class="delete-btn"></button>
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }}<span>${{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
